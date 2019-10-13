87f0625c2ad480381fb00deb3f580a688a3c81ed
Anthony Le Courric <freelance.anthony@gmail.com>
2019-10-13 22:07:50 +0200

    init

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3
+
+WORKDIR /usr/src/app
+
+#ENV PATH="/venv/bin:$PATH"
+#RUN python3 -m venv venv
+
+RUN ["apt-get", "update"]
+RUN ["apt-get", "install", "-y", "vim"]
+
+COPY requirements-docker.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+WORKDIR /usr/src/app/Module1/
+
+ENTRYPOINT [ "python" ]
+CMD [ "main.py" ]
---
+++ Module1/main.py
@@ -0,0 +1,49 @@
+from flask import Flask, jsonify, request
+from blockchain import Blockchain
+import requests
+from uuid import uuid4
+from urllib.parse import urlparse
+
+app = Flask(__name__)
+blockchain = Blockchain()
+
+@app.route('/')
+def index():
+    return "Hello World"
+
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    proof = blockchain.proof_of_work(previous_block['proof'])
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+
+    response = {
+        'message': "Congrat, you mined your first block!",
+        'index': block['index'],
+        'timestamp': block['timestamp'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+@app.route('/get_chain', methods=['GET'])
+def get_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+@app.route('/is_chain_valid', methods=['GET'])
+def is_chain_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    response = {
+        'is_valid': is_valid,
+        'message': "Blockchain Valid!" if is_valid else "Blockchain is not valid!"
+    }
+
+    return jsonify(response), 200 if is_valid else 498
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
